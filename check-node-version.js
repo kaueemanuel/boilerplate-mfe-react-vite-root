fba9f91591b838de51b0d278e9a42e3e62fee96c
Kauê Brasil <kaue.malheiros@olist.com>
2023-12-18 12:41:02 -0300

    remove semver

--- check-node-version.js
+++ check-node-version.js
@@ -1,10 +1,10 @@
-import semver from "semver";
 import packageData from "./package.json" assert { type: "json" };
 
 const version = packageData.engines.node;
-if (!semver.satisfies(process.version, version)) {
+
+if (process.version !== version && process.version !== `v${version}`) {
   console.log(
-    `Required node version ${version} not satisfied with current version ${process.version}.`
+    `Required node version v${version} not satisfied with current version ${process.version}.`
   );
   console.log("Use command:", `\x1b[32m nvm use ${version}\x1b[0m`);
   process.exit(1);

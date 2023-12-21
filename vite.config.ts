7e4e9547ddc091bea060a15055683d94d1e8c048
Kauê Brasil <kaue.malheiros@olist.com>
2023-12-21 18:13:53 -0300

    envs

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,50 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 import federation from "@originjs/vite-plugin-federation"
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "root-app",
-      filename: "remoteEntry.js",
-      remotes: {
-        "remote-mfe": "http://localhost:3001/assets/remote-mfe-entry.js",
-      },
-      shared: ["react", "react-dom", "react-router-dom", "zustand"],
-    }),
-  ],
-  optimizeDeps: {
-    include: ["@emotion/styled"],
-  },
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
-  },
-  define: {
-    process: process,
-  },
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), "")
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "root-app",
+        filename: "remoteEntry.js",
+        remotes: {
+          "remote-mfe": {
+            external : env.REMOTE_MFE_URL,
+            externalType: "url",
+            format: "esm",
+            from: "vite"
+          },
+        },
+        shared: {
+          "react": "*",
+          "react-dom": "*",
+          "react-router-dom": "*",
+          "zustand": "*",
+        },
+      }),
+    ],
+    optimizeDeps: {
+      include: ["@emotion/styled"],
+    },
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+    server: {
+      port: +env.PORT,
+    },
+    preview: {
+      port: +env.PORT,
+    },
+    define: {
+      process: process,
+    },
+  }
 })

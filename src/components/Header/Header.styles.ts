bb8761afb9ae7567bfec1f2a097e1b1e49d5d3f5
Kauê Brasil <kaue.malheiros@olist.com>
2023-12-26 09:17:22 -0300

    header style

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,7 +1,29 @@
 import styled from "@emotion/styled"
 
-export const Container = styled.header`
+export const Container = styled.div`
   display: flex;
   justify-content: left;
   align-items: center;
+  position: relative;
+
+  .navbar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 52px;
+    display: flex;
+    align-items: center;
+    background-color: #556cd6;
+    color: white;
+    gap: 16px;
+    padding: 0 16px;
+
+    a {
+      text-decoration: none;
+      color: white;
+      cursor: pointer;
+      font-weight: 600;
+    }
+  }
 `
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,19 @@
+import { NavLink } from "react-router-dom"
 import { Container } from "./Header.styles"
+import { useTranslation } from "react-i18next"
 
 interface Props extends React.HtmlHTMLAttributes<HTMLDivElement> {
   children?: React.ReactElement | string;
 }
 
 const Header = (props: Props) => {
+  const { t } = useTranslation()
   return (
     <Container>
-      Header
+      <header className="navbar">
+        <NavLink to={"/"}>{t("home")}</NavLink>
+        <NavLink to={"/remote"}>{t("remote")} 1</NavLink>
+      </header>
       {props?.children}
     </Container>
   )

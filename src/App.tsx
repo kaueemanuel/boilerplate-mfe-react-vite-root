feb59708af7c748c6ceb440125cd492fce71d3bd
Kauê Brasil <kaue.malheiros@olist.com>
2023-12-26 13:59:33 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import ExampleModuleScreen from "./screens/ScreenDefault"
-import Header from "./components/Header/Header"
-import { Suspense, lazy } from "react"
-
-const RemoteApp = lazy(() => import("remote-mfe/App"))
+import Routes from "./routes/routes"
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Header />
-              <ExampleModuleScreen />
-            </>
-          }
-        />
-        <Route
-          path="/remote/*"
-          element={
-            <>
-              <Suspense>
-                <Header />
-                <RemoteApp />
-              </Suspense>
-            </>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes />
   )
 }
 
---
+++ src/routes/routes.tsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ExampleModuleScreen from "../screens/ScreenDefault"
+import Header from "../components/Header/Header"
+import { Suspense, lazy } from "react"
+
+const RemoteApp = lazy(() => import("remote-mfe/App"))
+
+const AllRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <ExampleModuleScreen />
+            </>
+          }
+        />
+        <Route
+          path="/remote/*"
+          element={
+            <>
+              <Suspense>
+                <Header />
+                <RemoteApp />
+              </Suspense>
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AllRoutes
